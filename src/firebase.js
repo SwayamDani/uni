106e3b27dfd4c7cd64cb9c359b7c5302a2a2f6fa
tanicflux <151010399+tanicflux@users.noreply.github.com>
2024-11-22 17:41:38 -0800

    firebaseui

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
-import { getAuth, GoogleAuthProvider } from 'firebase/auth';
+import { getAuth, GoogleAuthProvider, EmailAuthProvider } from 'firebase/auth';
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -16,5 +16,6 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const auth = getAuth(app);
 const googleProvider = new GoogleAuthProvider();
+const emailProvider = new EmailAuthProvider();
 
 export { db, auth, googleProvider };
